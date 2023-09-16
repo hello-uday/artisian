18879cc27d2a202ade967cbde0fda0784b3c51a7
Uday Pratap Singh <udaypratapsingh95802@gmail.com>
2023-09-16 10:16:33 +0530

    start homepage

--- src/pages/login/Loginpage.js
+++ src/pages/login/Loginpage.js
@@ -14,15 +14,15 @@ const Loginpage = () => {
   };
 
   return (
-    <div>
+    <div className='body'>
       <h2>
         Artisian Welcomes You All
       </h2>
     <div className={`main-container ${isRightPanelActive ? 'right-panel-active' : ''}`} id="main-container">
-      <div class="form-main-container sign-up-main-container">
+      <div className="form-main-container sign-up-main-container">
         <form>
           <h1>Create Account</h1>
-          <div class="public-main-container">
+          <div className="public-main-container">
           </div>
           <span>use your own email</span>
           <input type="text" name="name" placeholder="Name" />
@@ -31,10 +31,10 @@ const Loginpage = () => {
           <button type="submit">Sign Up</button>
         </form>
       </div>
-      <div class="form-main-container sign-in-main-container">
+      <div className="form-main-container sign-in-main-container">
         <form>
           <h1>Sign in</h1>
-          <div class="public-main-container">
+          <div className="public-main-container">
           </div>
           <span>use your own credentials</span>
           <input type="email" name="email" placeholder="Email" />
@@ -43,16 +43,16 @@ const Loginpage = () => {
           <button>Sign In</button>
         </form>
       </div>
-      <div class="layerup-main-container">
-        <div class="layerup">
-          <div class="layerup-panel layerup-left">
+      <div className="layerup-main-container">
+        <div className="layerup">
+          <div className="layerup-panel layerup-left">
             <h1>Welcome Back!</h1>
             <p>
               For Sign In click here
             </p>
             <button className="ghost" onClick={handleSignInClick}>Sign In</button>
           </div>
-          <div class="layerup-panel layerup-right">
+          <div className="layerup-panel layerup-right">
             <h1>Hello, User</h1>
             <p>For Sign Up click here </p>
             <button className="ghost" onClick={handleSignUpClick}>Sign Up</button>
